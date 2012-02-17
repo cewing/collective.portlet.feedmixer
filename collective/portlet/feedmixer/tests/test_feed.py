c940845586f76b9412d0800bfa65e983dc37d6b7
cewing <cris@crisewing.com>
2012-02-16 19:36:41 -0800

    update tests to account for new format of 'entries' output

--- collective/portlet/feedmixer/tests/test_feed.py
+++ collective/portlet/feedmixer/tests/test_feed.py
@@ -7,7 +7,9 @@ from collective.portlet.feedmixer.portlet import Assignment
 from collective.portlet.feedmixer.tests import FEED_ONE
 from collective.portlet.feedmixer.tests import FEED_TWO
 
+
 class ConstructionTests(unittest.TestCase):
+
     def testInterface(self):
         feed=Assignment()
         verifyObject(IFeedMixer, feed)
@@ -28,8 +30,8 @@ class ConstructionTests(unittest.TestCase):
         self.assertEqual(feed.cache_timeout, "cache_timeout")
 
 
-
 class UtilityMethodTests(unittest.TestCase):
+
     def setUp(self):
         self.feed=Assignment(feeds="%s %s" % (FEED_ONE, FEED_TWO))
         self.parsed_one=feedparser.parse(FEED_ONE)
@@ -48,14 +50,26 @@ class UtilityMethodTests(unittest.TestCase):
         self.assertEqual(self.feed.mergeEntriesFromFeeds(False), [])
         self.assertEqual(self.feed.mergeEntriesFromFeeds([]), [])
 
+    def testMergedFeedsStructure(self):
+        merged = self.feed.mergeEntriesFromFeeds([self.parsed_one])
+        self.assertEqual(len(merged), 1)
+        self.assertTrue('title' in merged[0])
+        self.assertTrue('entries' in merged[0])
+
     def testMergeSingleFeed(self):
-        self.failUnless(self.feed.mergeEntriesFromFeeds([self.parsed_one]) is self.parsed_one.entries)
+        merged = self.feed.mergeEntriesFromFeeds([self.parsed_one])
+        entries = merged[0]['entries']
+        self.assertTrue(entries == self.parsed_one.entries)
 
     def testMergeTwoFeeds(self):
-        merged=self.feed.mergeEntriesFromFeeds([self.parsed_one, self.parsed_two])
-        self.assertEqual(len(merged),
-                len(self.parsed_one.entries)+len(self.parsed_two.entries))
-        links=[entry.link for entry in merged]
+        merged = self.feed.mergeEntriesFromFeeds(
+            [self.parsed_one, self.parsed_two])
+        self.assertEqual(len(merged), 1)
+        entries = merged[0]['entries']
+        self.assertEqual(
+            len(entries),
+            len(self.parsed_one.entries)+len(self.parsed_two.entries))
+        links=[entry.link for entry in entries]
         self.assertEqual(links, [
             u"http://test.two/item/1",
             u"http://test.one/item/1",
@@ -63,7 +77,18 @@ class UtilityMethodTests(unittest.TestCase):
             u"http://test.one/item/2",
             u"http://test.two/item/3",
             u"http://test.one/item/3"])
+        # merged feeds have an empty title
+        self.assertEqual(merged[0]['title'], u'')
 
+    def testUnmergedFeeds(self):
+        self.feed.data.merge_feeds = False
+        feeds = [self.parsed_one, self.parsed_two]
+        merged = self.feed.mergeEntriesFromFeeds(feeds)
+        self.assertEqual(len(merged), 2)
+        for idx, expected_feed in enumerate(feeds):
+            actual_feed = merged[idx]
+            self.assertEqual(actual_feed['title'], expected_feed.feed.title)
+            self.assertEqual(actual_feed['entries'], expected_feed.entries)
 
 
 def test_suite():
